00c3b5b505209a7ec7fbefa24287fb1751f23c03
lesliepajuelo <l.l.pajuelo@gmail.com>
2015-10-06 11:32:26 -0700

    Complete some

--- src/underbar.js
+++ src/underbar.js
@@ -192,12 +192,31 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    
+    iterator = iterator || function(a) {return a};
+
+    return _.reduce(collection, function(match, val) {
+      if(!match) {
+        return false;
+      } else {
+        return iterator(val) ? true: false;
+      }
+    }, true);
+    
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    
+    iterator = iterator || function(a) {return a};
+
+    return !_.every(collection, function(val){
+      return !iterator(val)
+      
+    })
+
   };
 
 
@@ -220,6 +239,9 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    var result = {};
+
+    console.log(arguments);
   };
 
   // Like extend, but doesn't ever overwrite a key that already
