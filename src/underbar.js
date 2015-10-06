f09f4d1d1f99a6642b01f9b78ce3fad26bfd4a72
lesliepajuelo <l.l.pajuelo@gmail.com>
2015-10-06 14:39:04 -0700

    Complete _.extend

--- src/underbar.js
+++ src/underbar.js
@@ -239,14 +239,19 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
-    var result = {};
-
-    console.log(arguments);
+    for(var i = 1; i < arguments.length; i++){
+      var object = arguments[i];
+      for(var key in object){
+        obj[key] = object[key];
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+
   };
 
 
